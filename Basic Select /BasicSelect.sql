9716fd2af1a0c598ef63840332567d7dc93c56c8
Mithilesh Parab <parab.mithilesh09@gmail.com>
2017-03-02 23:55:38 +0530

    Update BasicSelect.sql

--- Basic Select /BasicSelect.sql
+++ Basic Select /BasicSelect.sql
@@ -1,19 +1,19 @@
-//Query all columns (attributes) for every row in the CITY table.
+/*Query all columns (attributes) for every row in the CITY table.*/
 select * from CITY ;
 
 /*Query the names of all American cities in CITY with populations larger than 120000. The CountryCode for America is USA.*/
 Select NAME from CITY where POPULATION > 120000 and COUNTRYCODE='USA';
 
-//Query all columns for a city in CITY with the ID 1661.
+/*Query all columns for a city in CITY with the ID 1661.*/
 select * from CITY where ID=1661;
 
-//Query all attributes of every Japanese city in the CITY table. The COUNTRYCODE for Japan is JPN.
+/*Query all attributes of every Japanese city in the CITY table. The COUNTRYCODE for Japan is JPN.*/
 select * from CITY where COUNTRYCODE='JPN';
 
-//Query the names of all the Japanese cities in the CITY table. The COUNTRYCODE for Japan is JPN.
+/*Query the names of all the Japanese cities in the CITY table. The COUNTRYCODE for Japan is JPN.*/
 select NAME from CITY where COUNTRYCODE='JPN';
 
-//Query a list of CITY and STATE from the STATION table.
+/*Query a list of CITY and STATE from the STATION table.*/
 select CITY, STATE from STATION;
 
 /*Query a list of CITY names from STATION with even ID numbers only. You may print the results in any order, but must exclude duplicates from your answer.*/
